74fc6769e18daa066e8874ee1da5f37c1f45cf01
道奇(邰泳) <daoqi@i-i.ai>
2022-08-01 10:33:25 +0800

    feat: tips

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const log4js = require("log4js");
+log4js.configure({
+    appenders: { main: { type: "file", filename: "main.log" } },
+    categories: { default: { appenders: ["main"], level: "debug" } }
+});
+const logger = log4js.getLogger("main");
+logger.level = "debug";
 
+let mainWindow
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      nativeWindowOpen: false,
+      contextIsolation: false,
+      webSecurity: false,
+      nodeIntegration: true,
+      devTools: true,
+      webviewTag: true,
+      spellcheck: false
     }
   })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+setInterval(()=>{
+  if (mainWindow) {
+    mainWindow.webContents.send('setInterval1s', 'setInterval1s')
+  } else {
+    logger.debug("setInterval1s 找不到主窗口");
+  }
+}, 1000)
+function oneSecond() {
+  setTimeout(() => {
+      if (mainWindow) {
+        mainWindow.webContents.send('setTimeout1s', 'setInterval1s')
+      } else {
+        logger.debug("setTimeout1s 找不到主窗口");
+      }
+      oneSecond();
+  }, 1000);
+}
+oneSecond(); 
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,6 +1,31 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const {ipcRenderer} = require('electron').ipcRenderer
+
+const log4js = require("log4js");
+log4js.configure({
+    appenders: { render: { type: "file", filename: "render.log" } },
+    categories: { default: { appenders: ["render"], level: "debug" } }
+});
+
+
+const logger = log4js.getLogger("render");
+logger.level = "debug";
+
+function oneSecond() {
+    setTimeout(() => {
+        logger.debug("render setTimeout 1s");
+        oneSecond();
+    }, 1000);
+}
+oneSecond();
+
+setInterval(()=>{
+    logger.debug("render setInterval 1s");
+}, 1000)
+
+ipcRenderer.on('setInterval1s', (event, message) => {
+    logger.debug("main setInterval1s 1s");
+})
+
+ipcRenderer.on('setTimeout1s', (event, message) => {
+    logger.debug("main setTimeout1s 1s");
+})
